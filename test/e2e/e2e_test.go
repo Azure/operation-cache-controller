a41bbf0190c0c8186b5e78900a0fdf07e91e6e03
DQ <dengqian0826@gmail.com>
2025-04-14 15:25:06 +1000

    feat: Increase Gomega output length for better test readability

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -26,9 +26,14 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/format"
 
 	"github.com/Azure/operation-cache-controller/test/utils"
 )
+
+func init() {
+	format.MaxLength = 10000 // Or however much you need
+}
 
 // namespace where the project is deployed in
 const namespace = "operation-cache-controller-system"
